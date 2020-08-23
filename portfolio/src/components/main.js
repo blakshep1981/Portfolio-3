41828a6a14b00ad44be16e80e610dd8a8ecadee0
Ryan Simpkins <rythsimpkins@yahoo.com>
2020-08-22 18:34:13 -0700

    2nd

---
+++ portfolio/src/components/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import About from './about';
+import Contact from './contact';
+import Projects from './projects';
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
+    </Switch>
+)
+
+export default Main;
